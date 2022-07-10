2e1885e3e985f5488016933ad8e459607fba50a9
Jason Jeon <john40644064@gmail.com>
2022-07-10 20:08:52 +0900

    Initial Commit

---
+++ Redux-like Architecture/Redux_like_ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Redux_like_ArchitectureApp.swift
+//  Redux-like Architecture
+//
+//  Created by Jason Jeon on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Redux_like_ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
